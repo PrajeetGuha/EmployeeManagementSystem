0c74167f947b40205505fb56e36dae80dded5a87
prajeetg <prajeetg@trainee.nrifintech.com>
2023-03-27 16:54:16 +0530

    web controller test done

--- src/test/java/org/antwalk/ems/controller/WebControllerTest.java
+++ src/test/java/org/antwalk/ems/controller/WebControllerTest.java
@@ -1,6 +1,5 @@
 package org.antwalk.ems.controller;
 
-import org.antwalk.ems.security.AuthenticationSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
